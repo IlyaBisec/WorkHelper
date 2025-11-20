d061def33b233276330442d529abe8fb07543b84
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-11-20 15:26:17 +0300

    remove main window class

--- WorkHelper/include/WorkHelperMain.h
+++ WorkHelper/include/WorkHelperMain.h
@@ -1,33 +1,18 @@
+#ifndef _WORK_HELPER_APP_H_
+#define _WORK_HELPER_APP_H_
+#endif // !_WORK_HELPER_APP_H_
 #pragma once
 
-// WorkHelperMain.H - hedder for cpp, for main call func for init
-// 25/10/2025
+// WorkHelperApp.H - Main class of application, starts application
+// 30/10/2025
 // ilya_bisec (c) 2025-2026
 
-#include "wx/wx.h"
+#include "gui/MainWindowFrame.h"
 
-// Main window
-class MainWindow : public wxFrame
-{
-public:
-	MainWindow(const wxString &title)
-		: wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxSize(600, 400))
-	{
-		// Add main title on the centre
-		wxPanel *panel = new wxPanel(this, wxID_ANY);
-		new wxStaticText(panel, wxID_ANY, "Hallo workHelperApp!", wxPoint(20, 20), wxDefaultSize);
-	}
-};
 
-// App class
+
 class WorkHelperApp : public wxApp
 {
 public:
-	virtual bool OnInit() override
-	{
-		// Create main window
-		MainWindow *window = new MainWindow("Exaple wxWidjets C++17");
-		window->Show(true);
-		return true;
-	}
+    bool OnInit() override;
 };
