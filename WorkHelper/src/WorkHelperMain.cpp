76fe4f795db1343732dfa0aec1487c436e8f9058
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-11-20 15:28:18 +0300

    only start up func

--- WorkHelper/src/WorkHelperMain.cpp
+++ WorkHelper/src/WorkHelperMain.cpp
@@ -1,8 +1,12 @@
-
 #include "WorkHelperMain.h"
 
 
+bool WorkHelperApp::OnInit()
+{
+	MainWindowFrame *mainWindow = new MainWindowFrame();
+	mainWindow->Show();
+	return true;
+}
+
 // Lounch up app
 wxIMPLEMENT_APP(WorkHelperApp);
-
-
