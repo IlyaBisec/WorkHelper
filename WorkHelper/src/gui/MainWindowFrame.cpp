4588b7605f1b1a7a67bddc61f31442a215ff4d79
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2025-11-26 08:51:07 +0300

    add: event handlers

--- WorkHelper/src/gui/MainWindowFrame.cpp
+++ WorkHelper/src/gui/MainWindowFrame.cpp
@@ -4,10 +4,39 @@
 
 
 wxBEGIN_EVENT_TABLE(MainWindowFrame, wxFrame)
+    EVT_MENU(wxID_PASTE, MainWindowFrame::onPaste) // Ctrl + V
 wxEND_EVENT_TABLE()
 
 
-void MainWindowFrame::onAddFile(wxCommandEvent &)
+MainWindowFrame::MainWindowFrame()
+    : wxFrame(nullptr, wxID_ANY, "Work Helper", wxDefaultPosition,
+        wxSize(700, 500))
+{
+    wxBoxSizer *root = new wxBoxSizer(wxVERTICAL);
+
+    // Create button panel
+    m_toolbar = new ToolbarPanel(this);
+    root->Add(m_toolbar, 0, wxEXPAND | wxALL, 5);
+
+    // Create file explorer panel
+    m_list = new FileExplorerPanel(this);
+    root->Add(m_list, 1, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 5);
+
+    // Handlers
+    m_toolbar->btnAdd->Bind(wxEVT_BUTTON, &MainWindowFrame::onAdd, this);
+    m_toolbar->btnDelete->Bind(wxEVT_BUTTON, &MainWindowFrame::onDelete, this);
+    m_toolbar->btnRefresh->Bind(wxEVT_BUTTON, &MainWindowFrame::onRefresh, this);
+
+    // Ctrl + V Handler
+    Bind(wxEVT_MENU, &MainWindowFrame::onPaste, this, wxID_PASTE);
+
+    SetSizer(root);
+    Centre();
+}
+
+// ------------------- Handlers -----------------------------
+
+void MainWindowFrame::onAdd(wxCommandEvent &)
 {
     wxFileDialog dialog(
         this,
@@ -16,7 +45,7 @@ void MainWindowFrame::onAddFile(wxCommandEvent &)
         "",
         "Word documents (*.doc;*.docx)|*doc;*.docx|"
         "Excel documents (*.xls;*.xlsx)|*.xls;*.xlsx|"
-        "All files(*.*)|*.*", 
+        "All files(*.*)|*.*",
         wxFD_OPEN | wxFD_MULTIPLE
     );
 
@@ -32,7 +61,17 @@ void MainWindowFrame::onAddFile(wxCommandEvent &)
     }
 }
 
-void MainWindowFrame::onPase(wxCommandEvent &)
+void MainWindowFrame::onDelete(wxCommandEvent &evt)
+{
+    m_list->deleteSelection();
+}
+
+void MainWindowFrame::onRefresh(wxCommandEvent &evt)
+{
+    m_list->clear();
+}
+
+void MainWindowFrame::onPaste(wxCommandEvent &evt)
 {
     if (!wxTheClipboard->Open())
         return;
@@ -41,12 +80,22 @@ void MainWindowFrame::onPase(wxCommandEvent &)
     {
         wxFileDataObject data;
         wxTheClipboard->GetData(data);
-        
-        // Add all files from buffer
-        for (auto &file : data.GetFilenames())
-        {
-            m_list->addFile(file);
-        }
+
+        for (auto &filename : data.GetFilenames())
+            m_list->addFile(filename);
     }
+
     wxTheClipboard->Close();
+}
+
+
+void MainWindowFrame::initUI()
+{
+    wxBoxSizer *vert = new wxBoxSizer(wxVERTICAL);
+    
+    m_list = new FileExplorerPanel(this, 44);
+    vert->Add(m_list, 1, wxEXPAND | wxALL, 6);
+
+    wxBoxSizer *horiz = new wxBoxSizer(wxHORIZONTAL);
+
 }
